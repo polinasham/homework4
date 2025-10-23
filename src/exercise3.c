9f81c54369a98d392f8923c0691e8ec1c0866fb0
Полина Шамсутдинова <shamsutdinova.p@list.ru>
2025-10-24 00:11:56 +0700

    исправ

--- src/exercise3.c
+++ src/exercise3.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
-int main(int argc,char** argv) {
+
+int main() {
     float num[10];
+    int K = 2; // Можно делать ввод K через scanf, если нужно
     scanf("%f %f %f %f %f %f %f %f %f %f", &num[0], &num[1], &num[2], &num[3], &num[4], &num[5], &num[6], &num[7], &num[8], &num[9]);
-    int K = 2;
     
     if (K < 0) {
-        K = K + 10;
+        K += 10; // Обработка отрицательных K
     }
+    
     float print[10];
     for (int i = 0; i < 10; i++) {
         print[(i + K) % 10] = num[i];
     }
-
+    
     for (int i = 0; i < 10; i++) {
-        printf("%.3f ", print[i]);
+        printf("%.2f ", print[i]);
     }
+    printf("\n");
     return 0;
 }
