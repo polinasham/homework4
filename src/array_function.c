2935095212f5c9850f8c26c5bd150e217a9d8c0a
Полина Шамсутдинова <shamsutdinova.p@list.ru>
2025-10-30 20:20:48 +0700

    Реализация функций в array_function.c

--- src/array_function.c
+++ src/array_function.c
@@ -1,20 +1,18 @@
 #include "array_function.h"
-#include <stdio.h>
-#include <limits.h> 
 
 void join_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {
 
     for (size_t i = 0; i < size_src1; i++) {
         dest[i] = src1[i];
     }
-
-    for (size_t j = 0; j < size_src2; j++) {
-        dest[size_src1 + j] = src2[j];
+    for (size_t i = 0; i < size_src2; i++) {
+        dest[size_src1 + i] = src2[i];
     }
 }
 
 void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {
     size_t i = 0, j = 0, k = 0;
+
     while (i < size_src1 && j < size_src2) {
         if (src1[i] <= src2[j]) {
             dest[k++] = src1[i++];
@@ -22,7 +20,7 @@ void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t siz
             dest[k++] = src2[j++];
         }
     }
- 
+
     while (i < size_src1) {
         dest[k++] = src1[i++];
     }
@@ -34,14 +32,13 @@ void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t siz
 
 void get_min_and_max_from_int_array(int* src, size_t size, int* min, int* max) {
     if (size == 0 || src == NULL || min == NULL || max == NULL) {
-     
         return;
     }
 
-    int current_min = INT_MAX;
-    int current_max = INT_MIN;
+    int current_min = src[0];
+    int current_max = src[0];
 
-    for (size_t i = 0; i < size; i++) {
+    for (size_t i = 1; i < size; i++) {
         if (src[i] < current_min) {
             current_min = src[i];
         }
